ad280f58a17f9d67c77dbe8f1a63b53ee28dfa92
Mease, Cameron <cameron@pinknose.net>
2020-06-24 21:34:40 -0400

    Added extensions for arrays that allow subsets (ranges) of an array to be returned.  Makes up for missing range support in .NET (NOT Core) Framework

---
+++ Pinknose.Utilities.UnitTests/ArrayExtensionsTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pinknose.Utilities.UnitTests
+{
+    [TestClass]
+    public class ArrayExtensionsTests
+    {
+        [TestMethod]
+        public void ArrayRangeUnitTest()
+        {
+            int[] array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Assert.IsTrue(array.RangeByIndex(0, 10).SequenceEqual(array));
+            Assert.IsTrue(array.RangeByIndex(0, 0).SequenceEqual(new int[] { 0 }));
+            Assert.IsTrue(array.RangeByIndex(10, 10).SequenceEqual(new int[] { 10 }));
+            Assert.IsTrue(array.RangeByIndex(3, 5).SequenceEqual(new int[] { 3, 4, 5 }));
+
+            Assert.IsTrue(array.RangeByLength(0, 11).SequenceEqual(array));
+            Assert.IsTrue(array.RangeByLength(0, 1).SequenceEqual(new int[] { 0 }));
+            Assert.IsTrue(array.RangeByLength(10, 1).SequenceEqual(new int[] { 10 }));
+            Assert.IsTrue(array.RangeByLength(3, 3).SequenceEqual(new int[] { 3, 4, 5 }));
+
+            Assert.IsTrue(array.RangeFromEnd(11).SequenceEqual(array));
+            Assert.IsTrue(array.RangeFromEnd(1).SequenceEqual(new int[] { 10 }));
+            Assert.IsTrue(array.RangeFromEnd(3).SequenceEqual(new int[] { 8, 9, 10 }));
+
+            Assert.IsTrue(array.RangeExcludeLast(0).SequenceEqual(array));
+            Assert.IsTrue(array.RangeExcludeLast(1).SequenceEqual(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
+            Assert.IsTrue(array.RangeExcludeLast(3).SequenceEqual(new int[] { 0, 1, 2, 3, 4, 5, 6, 7 }));
+
+            Assert.IsTrue(array.RangeExcludeLast(0, 0).SequenceEqual(array));
+            Assert.IsTrue(array.RangeExcludeLast(1, 1).SequenceEqual(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }));
+            Assert.IsTrue(array.RangeExcludeLast(0, 10).SequenceEqual(new int[] { 0 }));
+
+            Assert.IsTrue(array.RangeExcludeFirst(0).SequenceEqual(array));
+            Assert.IsTrue(array.RangeExcludeFirst(1).SequenceEqual(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }));
+            Assert.IsTrue(array.RangeExcludeFirst(10).SequenceEqual(new int[] { 10 }));
+        }
+    }
+}
---
+++ Pinknose.Utilities/ArrayExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pinknose.Utilities
+{
+    public static class ArrayExtensions
+    {
+        public static T[] RangeByIndex<T>(this T[] array, int start, int end) where T: struct
+        {
+            if (start < 0 || start > array.Length - 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(start));
+            }
+
+            if (end < 0 || end > array.Length - 1 || end < start)
+            {
+                throw new ArgumentOutOfRangeException(nameof(start));
+            }
+
+            var newArray = new T[end - start + 1];
+
+            Array.Copy(array, start, newArray, 0, newArray.Length);
+
+            return newArray;
+        }
+
+        public static T[] RangeByLength<T>(this T[] array, int start, int length) where T : struct
+        {
+            if (start < 0 || start > array.Length - 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(start));
+            }
+
+            if (length < 0 || start+length > array.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(start));
+            }
+
+            var newArray = new T[length];
+
+            Array.Copy(array, start, newArray, 0, length);
+
+            return newArray;
+        }
+
+        public static T[] RangeFromEnd<T>(this T[] array, int fromEnd) where T : struct
+        {
+            if (fromEnd < 0 || fromEnd > array.Length)
+            {
+                throw new ArgumentOutOfRangeException(nameof(fromEnd));
+            }
+
+            var newArray = new T[fromEnd];
+
+            Array.Copy(array, array.Length - fromEnd, newArray, 0, fromEnd);
+
+            return newArray;
+        }
+
+        public static T[] RangeExcludeLast<T>(this T[] array, int exclude) where T : struct
+        {
+            return array.RangeByLength(0, array.Length - exclude);
+        }
+
+        public static T[] RangeExcludeLast<T>(this T[] array, int start, int exclude) where T : struct
+        {
+            return array.RangeByLength(start, array.Length - exclude - start);
+        }
+
+        public static T[] RangeExcludeFirst<T>(this T[] array, int exclude) where T : struct
+        {
+            return array.RangeByIndex(exclude, array.Length - 1);
+        }
+    }
+}
