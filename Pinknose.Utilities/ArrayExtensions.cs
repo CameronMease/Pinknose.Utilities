d81dc818630b43513200fdba661ef12d24e6caf6
Cameron Mease <cameron@pinknose.net>
2022-12-14 16:53:43 -0500

    Ran codemaid.

--- Pinknose.Utilities/ArrayExtensions.cs
+++ Pinknose.Utilities/ArrayExtensions.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/////////////////////////////////////////////////////////////////////////////////////////
+// MIT License
+//
+// Copyright (c) 2020-2022 Cameron Mease
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+/////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
 
 namespace Pinknose.Utilities
 {
     public static class ArrayExtensions
     {
-        public static T[] RangeByIndex<T>(this T[] array, int start, int end) where T: struct
+        #region Methods
+
+        public static T[] RangeByIndex<T>(this T[] array, int start, int end) where T : struct
         {
             if (start < 0 || start > array.Length - 1)
             {
@@ -32,7 +56,7 @@ namespace Pinknose.Utilities
                 throw new ArgumentOutOfRangeException(nameof(start));
             }
 
-            if (length < 0 || start+length > array.Length)
+            if (length < 0 || start + length > array.Length)
             {
                 throw new ArgumentOutOfRangeException(nameof(start));
             }
@@ -72,5 +96,7 @@ namespace Pinknose.Utilities
         {
             return array.RangeByIndex(exclude, array.Length - 1);
         }
+
+        #endregion Methods
     }
 }
