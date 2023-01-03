4aa1bbb000b64bf173fd61a9f4054f1d0e0d0bc3
Cameron Mease <cameron@pinknose.net>
2023-01-03 15:33:12 -0500

    Added Latex utitlity.

---
+++ Pinknose.Utilities/LatexUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinknose.Utilities
+{
+    public class LatexUtility
+    {
+        private static readonly char[] SpecialCharacters = new char[]{'#', '$', '%', '&', '~', '_', '^', '\\', '{', '}'};
+
+        public static string EncodeSpecialCharacters(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in text) 
+            { 
+                if (SpecialCharacters.Contains(c))
+                {
+                    sb.Append('\\');
+                }
+
+                sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
