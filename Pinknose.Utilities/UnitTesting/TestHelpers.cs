838f962f0c297a6ae97f2c4685ce2516c2344e37
Mease, Cameron <cameron@measesystems.com>
2024-03-04 10:36:54 -0500

    Added FstkAnalysisContext and refactored code so all objects are created within the context.

--- Pinknose.Utilities/UnitTesting/TestHelpers.cs
+++ Pinknose.Utilities/UnitTesting/TestHelpers.cs
@@ -32,6 +32,12 @@ namespace Pinknose.Utilities.UnitTesting
     {
         #region Methods
 
+        public static void AssertApproximatelyEqual<T>(this T value1, T value2, T acceptableDifference) where T : struct, IComparable<T>
+        {
+            var diff = Math.Abs((dynamic)value1 - (dynamic)value2);
+            Assert.IsTrue(diff <= acceptableDifference);
+        }
+
         public static void AssertContainsType<ListT, TargetT>(IEnumerable<ListT> list) => AssertContainsType(list, typeof(TargetT));
 
         public static void AssertContainsType<ListT>(IEnumerable<ListT> list, Type itemType) => Assert.IsTrue(list.Any(item => item?.GetType() == itemType));
@@ -41,20 +47,15 @@ namespace Pinknose.Utilities.UnitTesting
         public static void AssertDoesNotContainType<T>(IEnumerable<T> list, Type itemType) => Assert.IsFalse(list.Any(item => item?.GetType() == itemType));
 
 #if NETCOREAPP
-        public static void AssertApproximatelyEqual<T>(this T value1, T value2, T acceptableDifference) where T : struct, IComparable<T>
-        {
-            var diff = Math.Abs((dynamic)value1 - (dynamic)value2);
-            Assert.IsTrue(diff <= acceptableDifference);
-        }
 #else
 
-        public static void AssertApproximatelyEqual(this decimal value1, decimal value2, decimal acceptableDifference) 
+        public static void AssertApproximatelyEqual(this decimal value1, decimal value2, decimal acceptableDifference)
         {
             var diff = Math.Abs(value1 - value2);
             Assert.IsTrue(diff <= acceptableDifference);
         }
 
-        public static void AssertApproximatelyEqual(this float value1, float value2, float acceptableDifference) 
+        public static void AssertApproximatelyEqual(this float value1, float value2, float acceptableDifference)
         {
             var diff = Math.Abs(value1 - value2);
             Assert.IsTrue(diff <= acceptableDifference);
@@ -70,30 +71,6 @@ namespace Pinknose.Utilities.UnitTesting
         public static void AssertEqualWhenRounded(decimal expectedValue, decimal actualValue, int digits)
         {
             Assert.AreEqual(expectedValue, Math.Round(actualValue, digits));
-        }
-
-        public static void AssertExceptionCaught<T>(Action test) where T : Exception
-            => AssertExceptionCaught(typeof(T), test);
-
-        public static void AssertExceptionCaught(Type expectedExceptionType, Action test)
-        {
-            Assert.IsTrue(expectedExceptionType.IsAssignableTo(typeof(Exception)));
-
-            bool exceptionCaught = false;
-
-            try
-            {
-                test.Invoke();
-            }
-            catch (Exception ex)
-            {
-                if (ex.GetType() == expectedExceptionType)
-                {
-                    exceptionCaught = true;
-                }
-            }
-
-            Assert.IsTrue(exceptionCaught);
         }
 
         public static void AssertEventCaught<T>(T testObject, string eventName, Action test)
@@ -115,6 +92,30 @@ namespace Pinknose.Utilities.UnitTesting
             eventInfo.RemoveEventHandler(testObject, handler);
 
             Assert.IsTrue(eventCaught);
+        }
+
+        public static void AssertExceptionCaught<T>(Action test) where T : Exception
+                    => AssertExceptionCaught(typeof(T), test);
+
+        public static void AssertExceptionCaught(Type expectedExceptionType, Action test)
+        {
+            Assert.IsTrue(expectedExceptionType.IsAssignableTo(typeof(Exception)));
+
+            bool exceptionCaught = false;
+
+            try
+            {
+                test.Invoke();
+            }
+            catch (Exception ex)
+            {
+                if (ex.GetType() == expectedExceptionType)
+                {
+                    exceptionCaught = true;
+                }
+            }
+
+            Assert.IsTrue(exceptionCaught);
         }
 
         #endregion Methods
